3c7e15721f0cbdebbc75ef2d2718b1070a65a914
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-01-16 16:32:18 +0900

    xcb_connect

--- source/app.d
+++ source/app.d
@@ -1,6 +1,12 @@
-import std.stdio;
+import xcb.xcb;
+
+import std.experimental.logger;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  auto conn = xcb_connect(null, null);  // displayname, screenp
+  if (conn is null) {
+    fatal("Unable to connect to the X server.");
+  }
+  info("wmDerful has been started");
 }
