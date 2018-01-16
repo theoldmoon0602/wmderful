49cc865814d2a3d9322185630cd285f0711e2b24
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-01-16 16:42:47 +0900

    get screen

--- source/app.d
+++ source/app.d
@@ -4,9 +4,11 @@ import std.experimental.logger;
 
 void main()
 {
+  // start
   auto conn = xcb_connect(null, null);  // displayname, screenp
   if (conn is null) {
     fatal("Unable to connect to the X server.");
   }
+  auto screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
   info("wmDerful has been started");
 }
